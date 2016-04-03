5d894555a322f1c3e77625abbcb96fa1e9617007
EbXpJ6bp- <ebxpj6bp@outlook.jp>
2016-04-03 16:18:25 +0900

    update

---
+++ assets/js/me/javascript.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+	$('.btn').on('click', function (event) {
+		event.stopPropagation();
+		event.preventDefault();
+		if ($(this).closest('aside').find(".view").is(":hidden")) {
+			$(this).addClass("highlight");
+			$(this).closest('aside').find(".view").show("slow");
+		}
+		else {
+			$(this).closest('aside').find(".view").hide("slow");
+			$(this).removeClass("highlight");
+		}
+	});
+});
